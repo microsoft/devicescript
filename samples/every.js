b4ef560694cbaf075fcfc66f19dd5b357bd912c9
Michal Moskal <michal@moskal.me>
2022-04-04 13:57:33 -0700

    add sample

---
+++ samples/every.js
@@ -0,0 +1,13 @@
+var lightBulb1 = roles.lightBulb()
+var button1 = roles.button()
+button1.down.subscribe(() => {
+    console.log("down")
+    lightBulb1.brightness.write(1)
+})
+button1.up.subscribe(() => {
+    console.log("up")
+    lightBulb1.brightness.write(0)
+})
+every(5, () => {
+    console.log("5 second")
+})
