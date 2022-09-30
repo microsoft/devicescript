06934d27637246cb525f7b8009ed8b04be3c0b5f
Michal Moskal <michal@moskal.me>
2022-09-29 17:58:01 -0700

    move stuff around

--- runtime/devicescript/jacs_vm_internal.h
+++ runtime/devicescript/jacs_vm_internal.h
@@ -1,15 +1,5 @@
 #pragma once
 
-bool jacs_vm_args_ok(jacs_activation_t *frame, uint32_t localidx, uint32_t numargs);
-bool jacs_vm_args_and_fun_ok(jacs_activation_t *frame, uint32_t localidx, uint32_t numargs,
-                             uint32_t fidx);
-int32_t jacs_vm_fetch_int(jacs_activation_t *frame, jacs_ctx_t *ctx);
-static inline uint8_t jacs_vm_fetch_byte(jacs_activation_t *frame, jacs_ctx_t *ctx) {
-    if (frame->pc < frame->maxpc)
-        return ctx->img.data[frame->pc++];
-    jacs_runtime_failure(ctx, 60110);
-    return 0;
-}
 value_t jacs_vm_pop_arg(jacs_ctx_t *ctx);
 uint32_t jacs_vm_pop_arg_u32(jacs_ctx_t *ctx);
 uint32_t jacs_vm_pop_arg_i32(jacs_ctx_t *ctx);
@@ -19,3 +9,8 @@ void *jacs_vm_pop_arg_buffer_data(jacs_ctx_t *ctx, unsigned *sz);
 unsigned jacs_vm_pop_arg_stridx(jacs_ctx_t *ctx);
 unsigned jacs_vm_pop_arg_role(jacs_ctx_t *ctx);
 jacs_map_t *jacs_vm_pop_arg_map(jacs_ctx_t *ctx, bool create);
+
+extern const void *jacs_vm_op_handlers[];
+
+typedef void (*jacs_vm_stmt_handler_t)(jacs_activation_t *frame, jacs_ctx_t *ctx);
+typedef value_t (*jacs_vm_expr_handler_t)(jacs_activation_t *frame, jacs_ctx_t *ctx);
--- runtime/devicescript/step_util.c
+++ runtime/devicescript/step_util.c
@@ -1,25 +1,14 @@
 #include "jacs_internal.h"
 #include "jacs_vm_internal.h"
 
-bool jacs_vm_args_ok(jacs_activation_t *frame, uint32_t localidx, uint32_t numargs) {
-    if (numargs > 16 || localidx > frame->func->num_locals ||
-        localidx + numargs > frame->func->num_locals) {
-        jacs_runtime_failure(frame->fiber->ctx, 60113);
-        return false;
-    }
-    return true;
+static inline uint8_t jacs_vm_fetch_byte(jacs_activation_t *frame, jacs_ctx_t *ctx) {
+    if (frame->pc < frame->maxpc)
+        return ctx->img.data[frame->pc++];
+    jacs_runtime_failure(ctx, 60110);
+    return 0;
 }
 
-bool jacs_vm_args_and_fun_ok(jacs_activation_t *frame, uint32_t localidx, uint32_t numargs,
-                                    uint32_t fidx) {
-    if (fidx >= jacs_img_num_functions(&frame->fiber->ctx->img)) {
-        jacs_runtime_failure(frame->fiber->ctx, 60114);
-        return false;
-    }
-    return jacs_vm_args_ok(frame, localidx, numargs);
-}
-
-int32_t jacs_vm_fetch_int(jacs_activation_t *frame, jacs_ctx_t *ctx) {
+static inline int32_t jacs_vm_fetch_int(jacs_activation_t *frame, jacs_ctx_t *ctx) {
     uint8_t v = jacs_vm_fetch_byte(frame, ctx);
     if (v < 0xF8)
         return v;
@@ -36,6 +25,58 @@ int32_t jacs_vm_fetch_int(jacs_activation_t *frame, jacs_ctx_t *ctx) {
     return n ? -r : r;
 }
 
+static inline void jacs_vm_push(jacs_ctx_t *ctx, value_t v) {
+    if (ctx->stack_top >= JACS_MAX_STACK_DEPTH)
+        jacs_runtime_failure(ctx, 60109);
+    else
+        ctx->the_stack[ctx->stack_top++] = v;
+}
+
+void jacs_vm_exec_stmt(jacs_activation_t *frame) {
+    jacs_ctx_t *ctx = frame->fiber->ctx;
+
+    uint8_t op = jacs_vm_fetch_byte(frame, ctx);
+
+    if (op >= JACS_DIRECT_CONST_OP) {
+        jacs_vm_push(ctx,
+                     jacs_value_from_int(op - JACS_DIRECT_CONST_OP - JACS_DIRECT_CONST_OFFSET));
+        return;
+    }
+
+    if (op >= JACS_OP_PAST_LAST) {
+        jacs_runtime_failure(ctx, 60122);
+    } else {
+        uint8_t flags = JACS_OP_PROPS[op];
+
+        if (flags & JACS_BYTECODEFLAG_TAKES_NUMBER) {
+            ctx->literal_int = jacs_vm_fetch_int(frame, ctx);
+        }
+
+        uint8_t numargs = flags & JACS_BYTECODEFLAG_NUM_ARGS_MASK;
+
+        if (numargs) {
+            int bot = ctx->stack_top - numargs;
+            if (bot < 0)
+                jacs_runtime_failure(ctx, 60134);
+            ctx->arg_stack_bottom = bot;
+        }
+
+        if (flags & JACS_BYTECODEFLAG_IS_STMT) {
+            ((jacs_vm_stmt_handler_t)jacs_vm_op_handlers[op])(frame, ctx);
+            ctx->stack_top -= numargs;
+            if (ctx->stack_top)
+                jacs_runtime_failure(ctx, 60135);
+        } else {
+            value_t v = ((jacs_vm_expr_handler_t)jacs_vm_op_handlers[op])(frame, ctx);
+            ctx->stack_top -= numargs;
+            jacs_vm_push(ctx, v);
+        }
+    }
+}
+
+//
+// Pop utils
+//
 
 uint32_t jacs_vm_pop_arg_u32(jacs_ctx_t *ctx) {
     // TODO int vs uint?
@@ -43,7 +84,6 @@ uint32_t jacs_vm_pop_arg_u32(jacs_ctx_t *ctx) {
 }
 
 uint32_t jacs_vm_pop_arg_i32(jacs_ctx_t *ctx) {
-    // TODO specialize?
     return jacs_value_to_int(jacs_vm_pop_arg(ctx));
 }
 
@@ -117,3 +157,11 @@ jacs_map_t *jacs_vm_pop_arg_map(jacs_ctx_t *ctx, bool create) {
 
     return obj;
 }
+
+value_t jacs_vm_pop_arg(jacs_ctx_t *ctx) {
+    if (ctx->arg_stack_bottom >= ctx->stack_top)
+        return jacs_runtime_failure(ctx, 60108);
+
+    return ctx->the_stack[ctx->arg_stack_bottom++];
+}
+
