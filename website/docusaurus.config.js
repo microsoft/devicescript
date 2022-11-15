c8ab7f2e09076cf43154f619028cfeab86becf6e
pelikhan <jhalleux@microsoft.com>
2022-11-14 17:16:18 -0800

    prepping for github pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,11 +13,13 @@ const config = {
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "throw",
     favicon: "img/favicon.ico",
+    trailingSlash: false,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "microsoft", // Usually your GitHub org/user name.
     projectName: "jacscript", // Usually your repo name.
+    deploymentBranch: "gh-pages",
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
