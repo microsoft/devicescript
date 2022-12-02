c1b54ed02e72b72bc560b3f5a7ed4ea2d4207930
pelikhan <jhalleux@microsoft.com>
2022-12-02 12:29:23 -0800

    format

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -32,7 +32,11 @@ export async function mainCli() {
         .command("init")
         .description("configures the current directory for devicescript")
         .option("-f, --force", "force overwrite existing files")
-        .option("--spaces", "number of spaces when generating JSON (default 4)")
+        .option(
+            "--spaces <number>",
+            "number of spaces when generating JSON",
+            "4"
+        )
         .action(init)
 
     program
