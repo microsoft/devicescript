16de0b1b9c11e72adbb436e3301574dddeb72e68
Michal Moskal <michal@moskal.me>
2022-12-01 17:28:53 -0800

    add storage of settings in files (in native)

--- runtime/posix/jd_impl.c
+++ runtime/posix/jd_impl.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <sys/stat.h>
 
 #include "interfaces/jd_hw.h"
 
@@ -81,7 +82,57 @@ static setting_t *find_entry(const char *key, int create) {
     return p;
 }
 
+#ifndef __EMSCRIPTEN__
+int settings_in_files = 1;
+static FILE *open_key(const char *key, int wr) {
+    if (strchr(key, '/') || strchr(key, '\\') || strlen(key) > 32)
+        return NULL;
+    mkdir("tmp", 0777);
+    mkdir("tmp/settings", 0700);
+    char *path = jd_sprintf_a("tmp/settings/%s", key);
+    FILE *f = NULL;
+    if (wr == 2) {
+        unlink(path);
+    } else {
+        f = fopen(path, wr ? "w" : "r");
+    }
+    jd_free(path);
+    return f;
+}
+static int jd_settings_get_bin_f(const char *key, void *dst, unsigned space) {
+    FILE *f = open_key(key, 0);
+    if (!f)
+        return -1;
+    fseek(f, 0, SEEK_END);
+    unsigned size = ftell(f);
+    if (size > 0 && space >= size) {
+        fseek(f, 0, SEEK_SET);
+        int r = fread(dst, size, 1, f);
+        if (r != 1)
+            DMESG("rd err r=%d sz=%u", r, size);
+    }
+    fclose(f);
+    return size;
+}
+static int jd_settings_set_bin_f(const char *key, const void *val, unsigned size) {
+    int r = -1;
+    FILE *f = open_key(key, val == NULL ? 2 : 1);
+    if (val == NULL)
+        return 0;
+    if (f && fwrite(val, size, 1, f) == 1) {
+        r = 0;
+    }
+    if (f)
+        fclose(f);
+    return r;
+}
+#endif
+
 int jd_settings_get_bin(const char *key, void *dst, unsigned space) {
+#ifndef __EMSCRIPTEN__
+    if (settings_in_files)
+        return jd_settings_get_bin_f(key, dst, space);
+#endif
     setting_t *s = find_entry(key, 0);
     if (!s || !s->valptr)
         return -1;
@@ -94,6 +145,10 @@ int jd_settings_get_bin(const char *key, void *dst, unsigned space) {
 }
 
 int jd_settings_set_bin(const char *key, const void *val, unsigned size) {
+#ifndef __EMSCRIPTEN__
+    if (settings_in_files)
+        return jd_settings_set_bin_f(key, val, size);
+#endif
     setting_t *s = find_entry(key, 1);
     jd_free(s->valptr);
     if (val == NULL) {
