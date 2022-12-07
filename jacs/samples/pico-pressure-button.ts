4e434a1f1fa454c6be192d6812c7864069bc108f
pelikhan <jhalleux@microsoft.com>
2022-12-07 11:51:01 -0800

    move docs files

--- jacs/samples/pico-pressure-button.ts
+++ jacs/samples/pico-pressure-button.ts
@@ -1,8 +1,14 @@
 const sensor = roles.airPressure()
 const mouse = roles.hidMouse()
-sensor.pressure.onChange(100, pressure => {
+// listen for pressure changes
+sensor.pressure.onChange(10, () => {
+    const pressure = sensor.pressure.read()
+    console.log(pressure)
+    // user blows in straw
     if (pressure > 1400) {
+        // click!
         mouse.setButton(HidMouseButton.Left, HidMouseButtonEvent.Click)
+        // debouncing
         wait(0.05)
     }
 })
