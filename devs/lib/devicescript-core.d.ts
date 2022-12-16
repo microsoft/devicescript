b6b61fbc512bd4e8d243df830bd8ea75a2ff8bcb
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-12-16 08:53:21 -0800

    Api comments (#50)

--- devs/lib/devicescript-core.d.ts
+++ devs/lib/devicescript-core.d.ts
@@ -1,46 +1,107 @@
 /// <reference path="devicescript-spec.d.ts" />
 
 declare module "@devicescript/core" {
+
+    /**
+     * A base class for service clients
+     */
+    // TODO: rename to serviceclient?
     export class Role {
+        // TODO: convert to `get connected() { ... }`
         isConnected(): number
+        // TODO: is there a way to unregister an event?
         onConnected(handler: () => void): void
         onDisconnected(handler: () => void): void
     }
 
+    /**
+     * A base class for registers, events.
+     */
     export class PacketInfo {}
 
+    /**
+     * A base class for register clients.
+     */
     export class Register extends PacketInfo {}
 
+    /**
+     * A client for a register that holds a numerical value.
+     */
     export class RegisterNumber extends Register {
+        /**
+         * Gets the current value of the register as a number.
+         * TODO: missing value behavior (optional regs)
+         */
         read(): number
-        write(v: number): void
+
+        /**
+         * Sets the current value of the register.
+         * @param value value to assign to the register
+         * TODO: is it guaranteed, does it throw when it fails?
+         */
+        write(value: number): void
+
+        /**
+         * Registers a callback to execute when the register value changes by the given threshold
+         * @param threshold minimum value change required to trigger the handler
+         * @param handler callback to execute
+         * TODO: can we unregister?
+         */
         onChange(threshold: number, handler: (curr: number) => void): void
     }
 
+    /**
+     * A client for a register that holds a Buffer (byte[]).
+     */
     export class RegisterBuffer extends Register {
+        /**
+         * Gets the current value of the register.
+         */
         read(): Buffer
-        write(v: Buffer): void
+
+        /**
+         * Sets the current value of the register.
+         * @param value value to assign to the register
+         * TODO: is the buffer copied or owned?
+         * TODO: is Buffer = null same as buffer[0]
+         */
+        write(value: Buffer): void
+
+        /**
+         * Registers a callback to execute when the register value changes
+         * @param handler callback to execute
+         */
         onChange(handler: (curr: number) => void): void
     }
 
     export class RegisterBool extends Register {
         read(): boolean
-        write(v: boolean): void
+        write(value: boolean): void
         onChange(handler: (curr: number) => void): void
     }
 
+    /**
+     * A client for a register that holds a string value.
+     */
     export class RegisterString extends Register {
         read(): string
-        write(v: string): void
+        write(value: string): void
     }
 
     export class RegisterArray extends Register {
         read(): number[]
-        write(v: number[]): void
+        write(value: number[]): void
     }
 
     export class Event extends PacketInfo {
+        /**
+         * Blocks the current thread under the event is received.
+         */
         wait(): void
+        /**
+         * Register a callback that will be raised when the event is raised.
+         * @handler callback to execute
+         */
         subscribe(handler: () => void): void
     }
 
@@ -64,7 +125,9 @@ declare module "@devicescript/core" {
                 v7: number
             ) => void
         ): void
+        // TODO: delete?
         twin(path: string): number
+        // TODO: delete?
         onTwinChange(handler: () => void): void
     }
     export const cloud: CloudConnector
@@ -77,11 +140,20 @@ declare module "@devicescript/core" {
     export function reboot(): never
     export function onStart(handler: () => void): void
 
+    /**
+     * @internal
+     * TODO: move to submodule?
+     */
     export const packet: Buffer
 
     export { Buffer }
 
     global {
+        /**
+         * Compiles hex-encoded data as a buffer in flash
+         * @param lits hex-encoded string literal
+         * @param args 
+         */
         function hex(lits: any, ...args: any[]): Buffer
 
         class Buffer {
@@ -89,6 +161,9 @@ declare module "@devicescript/core" {
 
             static alloc(size: number): Buffer
 
+            /**
+             * Gets the length in bytes of the buffer
+             */
             length: number
             setLength(len: number): void
             getAt(offset: number, format: string): number
