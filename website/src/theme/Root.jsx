6a28cdad6cc419bd44055d27b564f23c44318164
pelikhan <jhalleux@microsoft.com>
2023-01-23 10:49:38 -0800

    root issue

--- website/src/theme/Root.jsx
+++ website/src/theme/Root.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import RootTheme from "@theme/Root"
+import RootTheme from "@theme-original/Root"
 import { DevToolsProvider } from "../contexts/DevToolsContext"
 // Default implementation, that you can customize
 export default function Root(props) {
