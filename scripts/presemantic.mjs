da1fc67bd3e41967f97f650d32cfc43b3cf719c6
pelikhan <jhalleux@microsoft.com>
2023-01-26 18:07:14 -0800

    more patching

--- scripts/presemantic.mjs
+++ scripts/presemantic.mjs
@@ -2,7 +2,9 @@ import { readFileSync, writeFileSync } from "node:fs"
 
 const fn = './package.json'
 const pkg = JSON.parse(readFileSync(fn, { encoding: 'utf-8' }))
-const i = pkg.workspaces.indexOf('jacdac-ts')
-if (i > -1)
-    pkg.workspaces.splice(i, 1)
+['runtime/jacdac-c/jacdac', 'jacdac-ts'].forEach(n => {
+    const i = pkg.workspaces.indexOf(n)
+    if (i > -1)
+        pkg.workspaces.splice(i, 1)
+})
 writeFileSync(fn, JSON.stringify(pkg, null, 4), { encoding: 'utf-8' })
