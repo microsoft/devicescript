96a58cd545004dae4f0c49af825ae84260b10b0a
pelikhan <jhalleux@microsoft.com>
2023-01-31 16:01:18 -0800

    adding fetch

--- vscode/src/CloudTreeDataProvider.ts
+++ vscode/src/CloudTreeDataProvider.ts
@@ -1,8 +1,15 @@
-import { CloudData, CloudManager, CloudNode, JDBus } from "jacdac-ts"
-import { JDomTreeDataProvider } from "./JDomTreeDataProvider"
+import {
+    CHANGE,
+    CloudData,
+    CloudManager,
+    CloudNode,
+    CLOUD_DEVICE_NODE,
+    CLOUD_SCRIPT_NODE,
+    JDBus,
+} from "jacdac-ts"
 import * as vscode from "vscode"
 import { ExtensionState } from "./state"
-import { idText } from "typescript"
+import "isomorphic-fetch"
 
 export interface CloudTreeItem extends vscode.TreeItem {}
 
@@ -10,7 +17,10 @@ export class CloudTreeDataProvider
     implements vscode.TreeDataProvider<CloudNode>
 {
     private _manager: CloudManager
+    private firstLoad = false
+
     constructor(readonly bus: JDBus, readonly state: ExtensionState) {
+        this.handleChange = this.handleChange.bind(this)
         const { context } = this.state
 
         // track config changes
@@ -45,7 +55,9 @@ export class CloudTreeDataProvider
     }
 
     async setApiRoot(apiRoot: string) {
-        await vscode.workspace.getConfiguration("devicescript.cloud").update("apiRoot", apiRoot)
+        await vscode.workspace
+            .getConfiguration("devicescript.cloud")
+            .update("apiRoot", apiRoot)
     }
 
     get token() {
@@ -63,27 +75,50 @@ export class CloudTreeDataProvider
         const token = await this.token
         const apiRoot = this.apiRoot
 
+        // unmount
+        this._manager?.off(CHANGE, this.handleChange)
+
         // mount new manager
-        if (token && apiRoot)
+        if (token && apiRoot) {
             this._manager = new CloudManager(this.bus, apiRoot, token)
-        else this._manager = undefined
+            this._manager.on(CHANGE, this.handleChange)
+            this.firstLoad = true
+        } else {
+            this._manager = undefined
+        }
 
         // refresh tree
+        this.refresh()
+    }
+
+    private handleChange() {
+        console.debug(`cloud: manager change`)
         this.refresh()
     }
 
     getTreeItem(node: CloudNode<CloudData>) {
         const id = `cloud:` + node.id
         const label = node.name
+        const contextValue = node.nodeKind
+        const icon = {
+            [CLOUD_DEVICE_NODE]: "circuit-board",
+            [CLOUD_SCRIPT_NODE]: "file-code",
+        }[contextValue]
         return <vscode.TreeItem>{
             id,
             label,
+            contextValue: contextValue,
+            iconPath: icon ?? new vscode.ThemeIcon(icon),
         }
     }
 
-    getChildren(element?: CloudNode) {
+    async getChildren(element?: CloudNode) {
         if (!element) {
             if (!this._manager) return undefined
+            if (this.firstLoad) {
+                this.firstLoad = false
+                this._manager.refresh()
+            }
             return [...this._manager.devices(), ...this._manager.scripts()]
         } else {
             return element?.children as CloudNode[]
