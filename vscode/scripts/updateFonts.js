1d344f1a89b1140c596422d5095e1e4845ee8ebb
pelikhan <jhalleux@microsoft.com>
2023-02-06 11:25:35 -0800

    fix: ensure built folder exists

--- vscode/scripts/updateFonts.js
+++ vscode/scripts/updateFonts.js
@@ -1,28 +1,28 @@
-const webfont = require('webfont');
-const fs = require('fs');
-const path = require('path');
+const webfont = require("webfont")
+const fs = require("fs-extra")
+const path = require("path")
 
-const svgs = [
-  "jacdac.svg",
-].map(name => path.join('.', 'icons', name));
+const svgs = ["jacdac.svg"].map(name => path.join(".", "icons", name))
 
 async function generateFont() {
-
-  try {
-    const result = await webfont.webfont({
-      files: svgs,
-      formats: ['woff'],
-      startUnicode: 0xE000,
-      verbose: false,
-      normalize: true,
-      sort: false
-    });
-    const dest = path.join('.', 'built', 'devicescript.woff')
-    fs.writeFileSync(dest, result.woff, 'binary');
-    console.log(`Font created at ${dest}`);
-  } catch (e) {
-    console.error('Font creation failed.', e);
-  }
+    try {
+        const result = await webfont.webfont({
+            files: svgs,
+            formats: ["woff"],
+            startUnicode: 0xe000,
+            verbose: false,
+            normalize: true,
+            sort: false,
+        })
+        fs.ensureDirSync("./built")
+        const dest = path.join("./built", "devicescript.woff")
+        fs.writeFileSync(dest, result.woff, "binary")
+        console.log(`Font created at ${dest}`)
+        process.exit(0)
+    } catch (e) {
+        console.error("Font creation failed.", e)
+        process.exit(-1)
+    }
 }
 
-generateFont();
+generateFont()
