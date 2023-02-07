5470bb99ddee11dba1a08794e85b18e98231d21c
pelikhan <jhalleux@microsoft.com>
2023-02-07 05:58:20 -0800

    device...script

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -20,7 +20,11 @@ function HomepageHeader() {
     return (
         <header className={clsx("hero hero--primary", styles.heroBanner)}>
             <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
+                <h1 className="hero__title">
+                    Device
+                    <wbr />
+                    Script
+                </h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}></div>
             </div>
