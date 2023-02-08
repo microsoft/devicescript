71d1d97abe74e1cadf512117504c980b1fc3168c
pelikhan <jhalleux@microsoft.com>
2023-02-07 21:49:28 -0800

    upgrade docusaurus

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -30,7 +30,7 @@ function HomepageHeader() {
                     <div className={styles.buttons}>
                         <Link
                             className="button button--secondary button--lg"
-                            to="/docs/intro"
+                            to="/intro"
                         >
                             Getting Started - 5min ⏱️
                         </Link>
