e46c2aebb8e96167e099d280b1fcdab0b655d9a3
pelikhan <jhalleux@microsoft.com>
2023-02-21 21:39:36 -0800

    updated imports

--- vscode/sampleprj/sim/app.mjs
+++ vscode/sampleprj/sim/app.mjs
@@ -1,10 +1,7 @@
-import * as os from "os"
 import { CONNECTION_STATE, createNodeWebSocketTransport, createWebSocketTransport, DEVICE_CHANGE, JDBus, SELF_ANNOUNCE } from "jacdac-ts"
 import "websocket-polyfill"
 import { Blob } from "buffer"
-global.Blob = Blob
-
-console.log(os.platform())
+globalThis.Blob = Blob
 
 const ws = createWebSocketTransport("ws://127.0.0.1:8081/")
 const bus = new JDBus([ws], { disableRoleManager: true, client: false })
