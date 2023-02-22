4be52ff2878eebb134af0037597ffecbe820420e
pelikhan <jhalleux@microsoft.com>
2023-02-22 11:11:04 -0800

    towards running typescript sim

--- vscode/sampleprj/sim/app.ts
+++ vscode/sampleprj/sim/app.ts
@@ -1,18 +1,26 @@
-import { AnalogSensorServer, CONNECTION_STATE, createWebSocketBus, DEVICE_CHANGE, SELF_ANNOUNCE, SRV_BUTTON, startServiceProviderFromServiceClass } from "jacdac-ts"
+import {
+    AnalogSensorServer,
+    CONNECTION_STATE,
+    createWebSocketBus,
+    DEVICE_CHANGE,
+    SELF_ANNOUNCE,
+} from "jacdac-ts"
 import { SRV_PSYCHOMAGNOTHERIC_ENERGY } from "../.devicescript/ts/constants"
 import "websocket-polyfill"
 import { Blob } from "buffer"
-globalThis.Blob = Blob
+globalThis.Blob = Blob as any
 
 const bus = createWebSocketBus()
 
 // simulator a customer service
 const server = new AnalogSensorServer(SRV_PSYCHOMAGNOTHERIC_ENERGY, {
     readingValues: [0.5],
-    readingError: [0.1]
+    readingError: [0.1],
 })
 bus.on(SELF_ANNOUNCE, () => {
-    server.reading.setValues([server.reading.values()[0] + (0.5 - Math.random())])
+    server.reading.setValues([
+        server.reading.values()[0] + (0.5 - Math.random()),
+    ])
 })
 
 bus.on([CONNECTION_STATE, DEVICE_CHANGE], () => {
