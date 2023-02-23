fb32e17742ec7084583e1ed5b09542dad8993cdb
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-02-22 20:27:03 -0800

    feat: node.js simulation support (#154)

--- cli/src/command.ts
+++ cli/src/command.ts
@@ -30,14 +30,15 @@ export function setConsoleColors(enabled: boolean) {
 
 // https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
 export function wrapColor(n: number | string, message: string) {
-    if (consoleColors)
-        return `\x1B[${n}m${message}\x1B[0m`
-    else
-        return message
+    if (consoleColors) return `\x1B[${n}m${message}\x1B[0m`
+    else return message
 }
 
 function wrapArgs(color: number, args: any[]) {
-    if (consoleColors && args.every(e => typeof e == "string" || typeof e == "number")) {
+    if (
+        consoleColors &&
+        args.every(e => typeof e == "string" || typeof e == "number")
+    ) {
         // if it's just strings & numbers use the coloring
         const msg = args.join(" ")
         return [wrapColor(color, msg)]
--- vscode/sampleprj/services/psychomagnothericenergy.md
+++ vscode/sampleprj/services/psychomagnothericenergy.md
@@ -1,6 +1,7 @@
 # Psychomagnotheric Energy
 
     identifier: 0x1f1dc7d5
+    extends: _sensor
 
 Measures the presence of ghosts in Ghostbusters.
 
@@ -9,3 +10,7 @@ Measures the presence of ghosts in Ghostbusters.
     ro energy_level: u0.16 / @ reading
 
 A measure of the presence of ghosts.
+
+    ro energy_level_error: u0.16 / @ reading_error
+
+Error on the measure.
