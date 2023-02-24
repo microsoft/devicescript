23853c1ff4f5150566947db0d3afd81320cd4bd5
pelikhan <jhalleux@microsoft.com>
2023-02-24 11:35:42 -0800

    more build fix

--- vscode/src/mainstatusbar.ts
+++ vscode/src/mainstatusbar.ts
@@ -1,4 +1,4 @@
-import { CHANGE, ConnectionState } from "jacdac-ts"
+import { CHANGE, ConnectionState, JDService } from "jacdac-ts"
 import * as vscode from "vscode"
 import { toMarkdownString } from "./catalog"
 import { Utils } from "vscode-uri"
@@ -27,6 +27,9 @@ export function activateMainStatusBar(
             currentFilename,
             currentDeviceScriptManager,
         } = extensionState.devtools
+        const deviceScriptManager = bus.node(
+            currentDeviceScriptManager
+        ) as JDService
         const connected = connectionState === ConnectionState.Connected
         const devices = bus.devices({
             ignoreInfrastructure: true,
@@ -73,7 +76,7 @@ ${
           )})`
         : ""
 } - entry point file<br/>
-${currentDeviceScriptManager?.qualifiedName || "..."} - deploy to device</br>
+${deviceScriptManager?.qualifiedName || "..."} - deploy to device</br>
 `)
         statusBarItem.text = [
             connectionState === ConnectionState.Connected
