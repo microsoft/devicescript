6bd828a5eb33e824fd3619384bab6bf9f204611e
Michal Moskal <michal@moskal.me>
2023-03-02 18:23:47 -0800

    cut down devs init, add devs add sim/service; fixes #165

--- cli/src/init.ts
+++ cli/src/init.ts
@@ -1,22 +1,121 @@
-import { CmdOptions, debug, GENDIR, LIBDIR, log } from "./command"
+import { CmdOptions, debug, fatal, GENDIR, LIBDIR, log } from "./command"
 import { dirname, join, resolve } from "node:path"
 import {
     pathExistsSync,
     writeFileSync,
     writeJSONSync,
-    emptyDirSync,
     readFileSync,
     ensureDirSync,
+    readJSONSync,
 } from "fs-extra"
-import { saveLibFiles } from "./build"
+import { build } from "./build"
 import { spawnSync } from "node:child_process"
-import { resolveBuildConfig } from "@devicescript/compiler"
+import { assert, randomUInt } from "jacdac-ts"
 
-const MAIN = "main.ts"
+const MAIN = "src/main.ts"
 const GITIGNORE = ".gitignore"
 const IMPORT_PREFIX = `import * as ds from "@devicescript/core"`
+const IS_PATCH = "__isPatch__"
 
-const optionalFiles: Record<string, Object | string> = {
+type FileSet = Record<string, Object | string>
+
+const serviceFiles: FileSet = {
+    "services/README.md": `# Services
+
+    Add custom service definition in this folder.
+    
+    -   [Read documentation](http://microsoft.github.io/devicescript/developer/custom-services)
+    `,
+}
+
+const simFiles: FileSet = {
+    ".vscode/launch.json": {
+        [IS_PATCH]: true,
+        configurations: [
+            {
+                name: "Sim",
+                request: "launch",
+                runtimeArgs: ["-r", "ts-node/register"],
+                args: ["${workspaceFolder}/sim/app.ts"],
+                skipFiles: ["<node_internals>/**"],
+                type: "node",
+                env: {
+                    TS_NODE_PROJECT: "${workspaceFolder}/sim/tsconfig.json",
+                },
+            },
+        ],
+        compounds: [
+            {
+                name: "DeviceScript+Sim",
+                configurations: ["DeviceScript", "Sim"],
+                stopAll: true,
+            },
+        ],
+    },
+    "package.json": {
+        [IS_PATCH]: true,
+        devDependencies: {
+            nodemon: "^2.0.20",
+            typescript: "^4.9.5",
+            "ts-node": "^10.9.1",
+        },
+        scripts: {
+            "build:sim": "cd sim && tsc --outDir ../.devicescript/sim",
+            build: "yarn build:devicescript && yarn build:sim",
+            "watch:sim":
+                "cd sim && nodemon --watch './**' --ext 'ts,json' --exec 'ts-node ./app.ts --project ./tsconfig.json'",
+            watch: "yarn watch:devicescript & yarn watch:sim",
+        },
+    },
+    "sim/runtime.ts": `import "websocket-polyfill"
+import { Blob } from "buffer"
+globalThis.Blob = Blob as any
+import customServices from "../.devicescript/services.json"
+import { createWebSocketBus } from "jacdac-ts"
+
+/**
+ * A Jacdac bus that will connect to the devicescript local server.
+ * 
+ * \`\`\`example
+ * import { bus } from "./runtime"
+ * \`\`\`
+ */
+export const bus = createWebSocketBus({
+    busOptions: {
+        services: customServices as jdspec.ServiceSpec[],
+    },
+})
+`,
+    "sim/README.md": `# Simulators (node.js)
+
+This folder contains a Node.JS/TypeScript application that will be executed side-by-side with
+the DeviceScript debugger and simulators. The application uses the [Jacdac TypeScript package](https://microsoft.github.io/jacdac-docs/clients/javascript/)
+to communicate with DeviceScript.
+
+The default entry point file is \`app.ts\`, which uses the Jacdac bus from \`runtime.ts\` to communicate
+with the rest of the DeviceScript execution.
+
+Feel free to modify to your needs and taste.
+`,
+    "sim/app.ts": `import { bus } from "./runtime"\n\n`,
+    "sim/tsconfig.json": {
+        type: "module",
+        compilerOptions: {
+            lib: ["es2022", "dom"],
+            module: "commonjs",
+            target: "es2022",
+            strict: true,
+            esModuleInterop: true,
+            skipLibCheck: true,
+            forceConsistentCasingInFileNames: true,
+            moduleResolution: "node",
+            resolveJsonModule: true,
+        },
+        include: ["./*.ts", "../node_modules/*"],
+    },
+}
+
+const optionalFiles: FileSet = {
     "src/tsconfig.json": {
         compilerOptions: {
             moduleResolution: "node",
@@ -53,27 +152,9 @@ const optionalFiles: Record<string, Object | string> = {
                 name: "DeviceScript",
                 type: "devicescript",
                 request: "launch",
-                program: "${workspaceFolder}/main.ts",
+                program: "${workspaceFolder}/" + MAIN,
                 deviceId: "${command:deviceScriptSimulator}",
                 stopOnEntry: false,
-            },
-            {
-                name: "Sim",
-                request: "launch",
-                runtimeArgs: ["-r", "ts-node/register"],
-                args: ["${workspaceFolder}/sim/app.ts"],
-                skipFiles: ["<node_internals>/**"],
-                type: "node",
-                env: {
-                    TS_NODE_PROJECT: "${workspaceFolder}/sim/tsconfig.json",
-                },
-            },
-        ],
-        compounds: [
-            {
-                name: "DeviceScript+Sim",
-                configurations: ["DeviceScript", "Sim"],
-                stopAll: true,
             },
         ],
     },
@@ -84,84 +165,21 @@ const optionalFiles: Record<string, Object | string> = {
         dependencies: {},
         devDependencies: {
             "@devicescript/cli": "*",
-            nodemon: "^2.0.20",
-            "ts-node": "^10.9.1",
         },
         scripts: {
-            setup: "devicescript init",
+            setup: "devicescript build", // generates .devicescript/lib/* files
             "build:devicescript": "devicescript build",
-            "build:sim": "cd sim && tsc --outDir ../.devicescript/sim",
-            build: "yarn build:devicescript && yarn build:sim",
-            "watch:devicescript": "devicescript devtools main.ts",
-            "watch:sim":
-                "cd sim && nodemon --watch './**' --ext 'ts,json' --exec 'ts-node ./app.ts --project ./tsconfig.json'",
-            watch: "yarn watch:devicescript & yarn watch:sim",
+            build: "yarn build:devicescript",
+            "watch:devicescript": `devicescript devtools ${MAIN}`,
+            watch: "yarn watch:devicescript",
             start: "yarn watch",
         },
     },
-    "boards/README.md": `# Boards
+    [MAIN]: `${IMPORT_PREFIX}
 
-This folder contains custom board definition files 
-to configure the DeviceScript firmware to your microcontroller
-pin and peripherical configuration.
-
--  [Read documentation](https://microsoft.github.io/devices/add-board)
-`,
-    "sim/runtime.ts": `
-import "websocket-polyfill"
-import { Blob } from "buffer"
-globalThis.Blob = Blob as any
-import customServices from "../.devicescript/services.json"
-import { createWebSocketBus } from "jacdac-ts"
-
-/**
- * A Jacdac bus that will connect to the devicescript local server.
- * 
- * \`\`\`example
- * import { bus } from "./runtime"
- * \`\`\`
- */
-export const bus = createWebSocketBus({
-    busOptions: {
-        services: customServices as jdspec.ServiceSpec[],
-    },
-})
-`,
-    "sim/README.md": `# Simulators (node.js)
-
-This folder contains a Node.JS/TypeScript application that will be executed side-by-side with
-the DeviceScript debugger and simulators. The application uses the [Jacdac TypeScript package](https://microsoft.github.io/jacdac-docs/clients/javascript/)
-to communicate with DeviceScript.
-
-The default entry point file is \`app.ts\`, which uses the Jacdac bus from \`runtime.ts\` to communicate
-with the rest of the DeviceScript execution.
-
-Feel free to modify to your needs and taste.
-`,
-    "sim/app.ts": `import { bus } from "./runtime"
-
-`,
-    "sim/tsconfig.json": {
-        type: "module",
-        compilerOptions: {
-            lib: ["es2022", "dom"],
-            module: "commonjs",
-            target: "es2022",
-            strict: true,
-            esModuleInterop: true,
-            skipLibCheck: true,
-            forceConsistentCasingInFileNames: true,
-            moduleResolution: "node",
-            resolveJsonModule: true,
-        },
-        include: ["./*.ts", "../node_modules/*"],
-    },
-    "services/README.md": `# Services
-
-Add custom service definition in this folder.
-
--   [Read documentation](http://microsoft.github.io/devicescript/developer/custom-services)
-`,
+ds.everyMs(1000, () => {
+    console.log(":)")
+})`,
     "README.md": `# - project name -
 
 This project uses [DeviceScript](https://microsoft.github.io/devicescript/).
@@ -170,7 +188,7 @@ This project uses [DeviceScript](https://microsoft.github.io/devicescript/).
 
 \`\`\`
 .devicescript      reserved folder for devicescript generated files
-main.ts            default DeviceScript entry point
+src/main.ts        default DeviceScript entry point
 ...
 /sim/app.ts        default node simulation entry point
 /sim/...
@@ -217,34 +235,53 @@ yarn watch
 -  navigate to devtools page (see terminal output) 
 to use the simulators or deploy to hardware.
 
--  open \`main.ts\` in your favorite TypeScript IDE and start editing.
+-  open \`src/main.ts\` in your favorite TypeScript IDE and start editing.
 
 `,
 }
+
 export interface InitOptions {
     force?: boolean
     spaces?: number
     install?: boolean
 }
 
-export async function init(
-    dir: string | undefined,
-    options: InitOptions & CmdOptions
-) {
-    const { force, spaces = 4, install } = options
+function patchJSON(fn: string, data: any) {
+    debug(`patch ${fn}`)
+    const existing = readJSONSync(fn)
+    const doPatch = (trg: any, src: any) => {
+        for (const k of Object.keys(src)) {
+            if (k == IS_PATCH) continue
+            if (trg[k] === undefined || typeof src[k] != "object")
+                trg[k] = src[k]
+            else if (Array.isArray(src[k]) && Array.isArray(trg[k])) {
+                for (const elt of src[k]) {
+                    assert(!!elt.name)
+                    if (!trg[k].find((e: any) => e.name == elt.name))
+                        trg[k].push(elt)
+                }
+            } else {
+                doPatch(trg[k], src[k])
+            }
+        }
+    }
+    doPatch(existing, data)
+    return existing
+}
+
+function writeFiles(dir: string, options: InitOptions, files: FileSet) {
+    const { spaces = 4 } = options
 
     const cwd = resolve(dir || "./")
-    log(`Configuring DeviceScript project`)
-
     ensureDirSync(cwd)
+    process.chdir(cwd) // just in case
 
-    const srcfolder = join(cwd, "src")
-    ensureDirSync(srcfolder)
-
-    Object.entries(optionalFiles).forEach(([fnr, data]) => {
+    Object.entries(files).forEach(([fnr, data]) => {
         // tsconfig.json
         const fn = join(cwd, fnr)
-        if (!pathExistsSync(fn) || force) {
+        const isPatch = typeof data == "object" && (data as any)[IS_PATCH]
+        if (isPatch) data = patchJSON(fn, data)
+        if (isPatch || !pathExistsSync(fn) || options.force) {
             debug(`write ${fn}`)
             const dn = dirname(fn)
             if (dn) ensureDirSync(dn)
@@ -256,13 +293,32 @@ export async function init(
         }
     })
 
-    // typescript definitions
-    const libdirn = join(cwd, LIBDIR)
-    emptyDirSync(libdirn)
-    debug(`write ${libdirn}/*`)
-    await saveLibFiles(resolveBuildConfig(), {
-        cwd,
-    })
+    return cwd
+}
+
+async function finishInit(cwd: string, options: InitOptions & CmdOptions) {
+    if (options.install) {
+        const npm = pathExistsSync(join(cwd, "package-lock.json"))
+        const cmd = npm ? "npm" : "yarn"
+        log(`install dependencies...`)
+        spawnSync(cmd, ["install"], {
+            shell: true,
+            stdio: "inherit",
+            cwd,
+        })
+    }
+
+    // build to get .devicescript/lib/* files etc
+    await build(MAIN, {})
+}
+
+export async function init(
+    dir: string | undefined,
+    options: InitOptions & CmdOptions
+) {
+    log(`Configuring DeviceScript project`)
+
+    const cwd = writeFiles(dir, options, optionalFiles)
 
     // .gitignore
     const gids = ["node_modules", GENDIR]
@@ -289,35 +345,71 @@ export async function init(
         }
     }
 
-    // main.ts
-    const mainn = join(srcfolder, MAIN)
-    if (!pathExistsSync(mainn)) {
-        debug(`write ${mainn}`)
-        writeFileSync(
-            mainn,
-            `${IMPORT_PREFIX}\n\nds.everyMs(1000, () => {\n    console.log(":)")\n})\n`,
-            {
-                encoding: "utf8",
-            }
-        )
-    }
-
-    if (install) {
-        const npm = pathExistsSync(join(cwd, "package-lock.json"))
-        const cmd = npm ? "npm" : "yarn"
-        log(`install dependencies...`)
-        spawnSync(cmd, ["install"], {
-            shell: true,
-            stdio: "inherit",
-            cwd,
-        })
-    }
+    await finishInit(cwd, options)
 
     // help message
     log(``)
-    log(`Your DeviceScript project is initialized.`)
+    log(
+        `Your DeviceScript project is initialized. Try 'devs add' to see what can be added.`
+    )
     log(
         `To get more help, https://microsoft.github.io/devicescript/getting-started/ .`
     )
+    log(``)
+}
+
+export async function addSim(options: InitOptions & CmdOptions) {
+    log(`Adding simulator support`)
+
+    const cwd = writeFiles(".", options, simFiles)
+
+    await finishInit(cwd, options)
+
+    // help message
+    log(``)
+    log(`Simulator support added.`)
+    log(``)
+}
+
+export interface AddServiceOptions extends InitOptions {
+    name: string
+}
+
+export async function addService(options: AddServiceOptions) {
+    const { name } = options
+    if (!name) fatal("--name argument required; example: --name 'Light Level'")
+
+    const id = options.name.toLowerCase().replace(/\s+/g, "")
+
+    log(`Adding service "${name}"`)
+
+    const num = randomUInt(0xfff_ffff) | 0x1000_0000
+
+    const files = Object.assign(
+        {
+            ["services/" + id + ".md"]: `# ${name}
+
+    identifier: 0x${num.toString(16)}
+    extends: _sensor
+
+Measures ${name}.
+
+## Registers
+
+    ro level: u0.16 / @ reading
+
+A measure of ${name}.
+`,
+        },
+        serviceFiles
+    )
+
+    const cwd = writeFiles(".", options, files)
+
+    await finishInit(cwd, options)
+
+    // help message
+    log(``)
+    log(`Service added ${id}.`)
     log(``)
 }
