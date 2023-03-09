3f397e382e3c0c6915cb5a191d1c27982711b78a
pelikhan <jhalleux@microsoft.com>
2023-03-08 20:25:02 -0800

    fix: added uploadObject, trackEvent

--- devs/samples/cloud.ts
+++ devs/samples/cloud.ts
@@ -4,6 +4,7 @@ const cloud = new ds.CloudAdapter()
 console.log(await cloud.connectionName.read())
 
 await ds.sleepMs(500)
+await cloud.trackEvent("started")
 await cloud.uploadJson(JSON.stringify({ foo: 1, bar: { baz: "foo" } }))
 await cloud.uploadBinary(hex`00 11 22 33`)
 
@@ -13,3 +14,7 @@ cloud.onJson.subscribe(val => {
 cloud.onBinary.subscribe(val => {
     console.log("got bin", val)
 })
+
+setInterval(async () => {
+    await cloud.trackEvent("alive")
+}, 30000)
