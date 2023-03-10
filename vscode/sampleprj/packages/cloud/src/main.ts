a2f48c4a3743f7c03093f7dc10aaf19c9fd5275e
pelikhan <jhalleux@microsoft.com>
2023-03-09 17:25:12 -0800

    add tests

--- vscode/sampleprj/packages/cloud/src/main.ts
+++ vscode/sampleprj/packages/cloud/src/main.ts
@@ -1,5 +1,34 @@
 import * as ds from "@devicescript/core"
+import { trackEvent, uploadMessage } from "."
+import { describe, test } from "@devicescript/test"
 
-setInterval(() => {
-    console.log(":)")
-}, 1000)
+console.log("start test")
+await trackEvent("cloud.test.start")
+
+describe("trackEvent", () => {
+    test("call", async () => {
+        await trackEvent("test.cloud")
+    })
+    test("properties", async () => {
+        await trackEvent("test.props", { properties: { a: "a" } })
+    })
+    test("measurements", async () => {
+        await trackEvent("test.mes", { measurements: { a: 1 } })
+    })
+})
+
+describe("upload message", () => {
+    test("upload", async () => {
+        await uploadMessage("cloud/tests", {
+            t: ds.millis(),
+        })
+    })
+    test("upload2", async () => {
+        await uploadMessage("cloud/tests", {
+            t: ds.millis(),
+        })
+        await uploadMessage("cloud/tests", {
+            t: ds.millis(),
+        })
+    })
+})
