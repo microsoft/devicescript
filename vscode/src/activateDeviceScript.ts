ef09d40d99da7e54d93b549e7d1f529970fc9041
pelikhan <jhalleux@microsoft.com>
2023-03-14 05:01:55 -0700

    fix: cleanup activation

--- vscode/src/activateDeviceScript.ts
+++ vscode/src/activateDeviceScript.ts
@@ -1,20 +1,10 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
-import {
-    Flags,
-    FRAME_PROCESS,
-    JDFrameBuffer,
-    serializeToTrace,
-} from "jacdac-ts"
 import * as vscode from "vscode"
-import type { SideOutputEvent } from "../../cli/src/sideprotocol"
 import { activateDebugger } from "./debugger"
 import { activateGateway } from "./gateway/activateGateway"
-import { startJacdacBus, stopJacdacBus, subSideEvent } from "./jacdac"
+import { startJacdacBus, stopJacdacBus } from "./jacdac"
 import { JDomDeviceTreeItem, activateTreeViews } from "./JDomTreeDataProvider"
 import { activateMainStatusBar } from "./mainstatusbar"
+import { activateOutputChannels, registerOutputChannel } from "./output"
 import { DeviceScriptExtensionState } from "./state"
 
 export function activateDeviceScript(context: vscode.ExtensionContext) {
@@ -78,20 +68,11 @@ export function activateDeviceScript(context: vscode.ExtensionContext) {
         vscode.commands.registerCommand(
             "extension.devicescript.boards.add",
             async () => extensionState.addBoard()
-        )
-    )
-
-    subscriptions.push(
+        ),
         vscode.commands.registerCommand(
             "extension.devicescript.variables.simulator",
             () => extensionState.simulatorScriptManagerId
-        )
-    )
-
-    activateDebugger(extensionState)
-    activateGateway(context, extensionState)
-
-    subscriptions.push(
+        ),
         vscode.commands.registerCommand(
             "extension.devicescript.simulator.stop",
             async () => {
@@ -110,30 +91,13 @@ export function activateDeviceScript(context: vscode.ExtensionContext) {
         )
     )
 
+    activateDebugger(extensionState)
+    activateGateway(context, extensionState)
     activateTreeViews(extensionState)
     activateMainStatusBar(extensionState)
 
     // packet trace
-    let jacdacPacketsOutputChannel: vscode.OutputChannel = undefined
-    const logFrame = (frame: JDFrameBuffer) => {
-        const output = jacdacPacketsOutputChannel
-        if (!output) return
-        const msg = serializeToTrace(frame, 0, bus, {})
-        if (msg) output.appendLine(msg)
-    }
-    // apply settings
-    const configure = () => {
-        Flags.diagnostics = !!jacdacConfig.get("diagnostics")
-        const tracePackets = !!jacdacConfig.get("tracePackets")
-        if (tracePackets) {
-            if (!jacdacPacketsOutputChannel)
-                jacdacPacketsOutputChannel =
-                    vscode.window.createOutputChannel("Jacdac Packets")
-            bus.on(FRAME_PROCESS, logFrame)
-        } else if (!tracePackets && jacdacPacketsOutputChannel) {
-            bus.off(FRAME_PROCESS, logFrame)
-        }
-    }
+    const configure = activateOutputChannels(bus, jacdacConfig)
 
     // hook up to configurations
     vscode.workspace.onDidChangeConfiguration(
@@ -148,32 +112,3 @@ export function activateDeviceScript(context: vscode.ExtensionContext) {
         extensionState.devtools.start()
     }
 }
-
-function registerOutputChannel(extensionMode: vscode.ExtensionMode) {
-    const output = vscode.window.createOutputChannel("DeviceScript", {
-        log: true,
-    })
-    subSideEvent<SideOutputEvent>("output", msg => {
-        const tag = msg.data.from
-        let fn = output.info
-        if (tag.endsWith("err")) fn = output.error
-        for (const l of msg.data.lines) fn(tag + ":", l)
-    })
-    const redirectConsoleOutput =
-        extensionMode == vscode.ExtensionMode.Production
-    if (redirectConsoleOutput) {
-        // note that this is local to this extension - see inject.js
-        console.debug = output.debug
-        console.log = output.info
-        console.warn = output.warn
-        console.error = output.error
-        console.info = console.log
-    }
-}
-/*
-class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
-	createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
-		return new vscode.DebugAdapterInlineImplementation(new LoggingDebugSession());
-	}
-}
-*/
