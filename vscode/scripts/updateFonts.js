fec4422e98903f005b3b15974dc81d798cefeb8d
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-03-24 13:05:47 -0700

    fix windows build (#266)

--- vscode/scripts/updateFonts.js
+++ vscode/scripts/updateFonts.js
@@ -2,10 +2,11 @@ const webfont = require("webfont")
 const fs = require("fs-extra")
 const path = require("path")
 
-const svgs = ["jacdac.svg"].map(name => path.join(".", "icons", name))
+const svgs = ["jacdac.svg"].map(name => `./icons/${name}`)
 
 async function generateFont() {
     try {
+        console.log(`build font for ${svgs}`)
         const result = await webfont.webfont({
             files: svgs,
             formats: ["woff"],
