7c4ea1b959b2d45950501229d0052b1c1d33ac4a
pelikhan <jhalleux@microsoft.com>
2023-04-24 15:40:49 -0700

    accessbiilty fixes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,171 @@
-// @ts-check
+// @ts-nocheck
 const { configure } = require("@rise4fun/docusaurus-plugin-rise4fun")
 
 const lightCodeTheme = require("prism-react-renderer/themes/github")
-const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark")
+
+const lightTheme = {
+  ...lightCodeTheme,
+  styles: [
+    ...lightCodeTheme.styles,
+    {
+      types: ['title'],
+      style: {
+        color: '#0550AE',
+        fontWeight: 'bold',
+      },
+    },
+    {
+      types: ['parameter'],
+      style: {
+        color: '#953800',
+      },
+    },
+    {
+      types: ['boolean', 'rule', 'color', 'number', 'constant', 'property'],
+      style: {
+        color: '#005CC5',
+      },
+    },
+    {
+      types: ['atrule', 'tag'],
+      style: {
+        color: '#22863A',
+      },
+    },
+    {
+      types: ['script'],
+      style: {
+        color: '#24292E',
+      },
+    },
+    {
+      types: ['operator', 'unit', 'rule'],
+      style: {
+        color: '#D73A49',
+      },
+    },
+    {
+      types: ['font-matter', 'string', 'attr-value'],
+      style: {
+        color: '#C6105F',
+      },
+    },
+    {
+      types: ['class-name'],
+      style: {
+        color: '#116329',
+      },
+    },
+    {
+      types: ['attr-name'],
+      style: {
+        color: '#0550AE',
+      },
+    },
+    {
+      types: ['keyword'],
+      style: {
+        color: '#CF222E',
+      },
+    },
+    {
+      types: ['function'],
+      style: {
+        color: '#8250DF',
+      },
+    },
+    {
+      types: ['selector'],
+      style: {
+        color: '#6F42C1',
+      },
+    },
+    {
+      types: ['variable'],
+      style: {
+        color: '#E36209',
+      },
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: '#6B6B6B',
+      },
+    },
+  ],
+};
+
+const darkTheme = {
+  plain: {
+    color: '#D4D4D4',
+    backgroundColor: '#212121',
+  },
+  styles: [
+    ...darkCodeTheme.styles,
+    {
+      types: ['title'],
+      style: {
+        color: '#569CD6',
+        fontWeight: 'bold',
+      },
+    },
+    {
+      types: ['property', 'parameter'],
+      style: {
+        color: '#9CDCFE',
+      },
+    },
+    {
+      types: ['script'],
+      style: {
+        color: '#D4D4D4',
+      },
+    },
+    {
+      types: ['boolean', 'arrow', 'atrule', 'tag'],
+      style: {
+        color: '#569CD6',
+      },
+    },
+    {
+      types: ['number', 'color', 'unit'],
+      style: {
+        color: '#B5CEA8',
+      },
+    },
+    {
+      types: ['font-matter'],
+      style: {
+        color: '#CE9178',
+      },
+    },
+    {
+      types: ['keyword', 'rule'],
+      style: {
+        color: '#C586C0',
+      },
+    },
+    {
+      types: ['regex'],
+      style: {
+        color: '#D16969',
+      },
+    },
+    {
+      types: ['maybe-class-name'],
+      style: {
+        color: '#4EC9B0',
+      },
+    },
+    {
+      types: ['constant'],
+      style: {
+        color: '#4FC1FF',
+      },
+    },
+  ],
+};
 
 const config = configure(
     {
@@ -119,8 +282,8 @@ const config = configure(
                     ],
                 },
                 prism: {
-                    theme: lightCodeTheme,
-                    darkTheme: darkCodeTheme,
+                    theme: lightTheme,
+                    darkTheme: darkTheme,
                 },
             }),
     },
