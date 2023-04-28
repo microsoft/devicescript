c93a4b555a625c512a3fc0f23f91659aeaf50a12
pelikhan <jhalleux@microsoft.com>
2023-04-28 15:15:29 -0700

    don't interop jdutils

--- compiler/src/magic.ts
+++ compiler/src/magic.ts
@@ -1,7 +1,17 @@
 import { BinFmt } from "./bytecode"
-import { read32 } from "./jdutil"
 
 export const IMAGE_MIN_LENGTH = 100
+
+function read32(buf: Uint8Array, pos: number) {
+    return (
+        (buf[pos] |
+            (buf[pos + 1] << 8) |
+            (buf[pos + 2] << 16) |
+            (buf[pos + 3] << 24)) >>>
+        0
+    )
+}
+
 
 export function checkMagic(img: Uint8Array) {
     return read32(img, 0) === BinFmt.MAGIC0 && read32(img, 4) === BinFmt.MAGIC1
