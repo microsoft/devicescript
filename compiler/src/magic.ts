c92bd7c5d69f0a22ba2a89bf77526b998a2746e6
pelikhan <jhalleux@microsoft.com>
2023-04-28 14:55:37 -0700

    typo

--- compiler/src/magic.ts
+++ compiler/src/magic.ts
@@ -4,5 +4,5 @@ import { read32 } from "./jdutil"
 export const IMAGE_MIN_LENGTH = 100
 
 export function checkMagic(img: Uint8Array) {
-    return read32(img, 0) === BinFmt.MAGIC0 && read32(img, 4) != BinFmt.MAGIC1
+    return read32(img, 0) === BinFmt.MAGIC0 && read32(img, 4) === BinFmt.MAGIC1
 }
