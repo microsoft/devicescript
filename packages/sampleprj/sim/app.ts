48500b38d465a24996430f6e8a78c9151b4d9002
pelikhan <jhalleux@microsoft.com>
2023-05-18 22:43:38 -0700

    add csv history

--- packages/sampleprj/sim/app.ts
+++ packages/sampleprj/sim/app.ts
@@ -1,21 +1,4 @@
-import { addServer, AnalogSensorServer } from "jacdac-ts"
-import { SRV_PSYCHOMAGNOTHERIC_ENERGY } from "../.devicescript/ts/constants"
-import { bus } from "./runtime"
+import "./auroscope"
+import { startHouse } from "./house"
 
-// simulator a customer service
-const server = new AnalogSensorServer(SRV_PSYCHOMAGNOTHERIC_ENERGY, {
-    readingValues: [0.5],
-    readingError: [0.1],
-    streamingInterval: 500,
-})
-// randomly change the reading value
-setInterval(() => {
-    const value = server.reading.values()[0]
-    const newValue = value + (0.5 - Math.random()) / 10
-    server.reading.setValues([newValue])
-    console.debug(`psycho value: ${newValue}`)
-}, 100)
-
-// mount server on bus to make it visible
-// to DeviceScript
-addServer(bus, "aurascope", server)
+startHouse()
---
+++ packages/sampleprj/sim/auroscope.ts
@@ -0,0 +1,21 @@
+import { addServer, AnalogSensorServer } from "jacdac-ts"
+import { SRV_PSYCHOMAGNOTHERIC_ENERGY } from "../.devicescript/ts/constants"
+import { bus } from "./runtime"
+
+// simulator a customer service
+const server = new AnalogSensorServer(SRV_PSYCHOMAGNOTHERIC_ENERGY, {
+    readingValues: [0.5],
+    readingError: [0.1],
+    streamingInterval: 500,
+})
+// randomly change the reading value
+setInterval(() => {
+    const value = server.reading.values()[0]
+    const newValue = value + (0.5 - Math.random()) / 10
+    server.reading.setValues([newValue])
+    //console.debug(`psycho value: ${newValue}`)
+}, 2000)
+
+// mount server on bus to make it visible
+// to DeviceScript
+addServer(bus, "aurascope", server)
