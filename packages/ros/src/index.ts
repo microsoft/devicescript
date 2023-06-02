0d53ae065e5dbd9b53f118a7bd85b102d259b955
pelikhan <jhalleux@microsoft.com>
2023-06-01 19:33:26 -0700

    ros api

---
+++ packages/ros/src/index.ts
@@ -0,0 +1,31 @@
+import * as ds from "@devicescript/core"
+
+type RosMessage = any
+
+/**
+ * Subscribes to a ROS topi.
+ * @param topic ROS topic
+ * @param handler callback function
+ * @returns callback to unsubscribe
+ */
+export function rosSubscribe<T extends RosMessage>(
+    topic: string,
+    handler: (msg: T) => ds.AsyncVoid
+): ds.Unsubscribe {
+    //
+    return undefined
+}
+
+/**
+ * Publishes a ROS message
+ * @param topic
+ * @param msg
+ * @returns
+ */
+export function rosPublish<T extends RosMessage>(
+    topic: string,
+    msg: T
+): ds.AsyncVoid {
+    //
+    return undefined
+}
