3d87887164aa8295cf63edb30a9f05a1eb8e3449
pelikhan <jhalleux@microsoft.com>
2023-06-05 21:02:06 -0700

    updated build status example

--- packages/sampleprj/src/maingithubbuildstatus.ts
+++ packages/sampleprj/src/maingithubbuildstatus.ts
@@ -1,5 +1,6 @@
 import { readSetting } from "@devicescript/settings"
 import { fetch } from "@devicescript/net"
+import { setStatusLight } from "@devicescript/runtime"
 
 // read configuration from ./env.defaults
 const owner = await readSetting("GITHUB_OWNER")
@@ -12,19 +13,20 @@ console.log({ owner, repo, ref })
 if (!owner || !repo) throw new Error("missing configuration")
 
 // track state of last fetch
-let state: "failure" | "pending" | "success" | "error" | "" = ""
+let state: "failure" | "pending" | "success" | "error" | "" = "success"
 let blinki = 0
 
 // update status light
-setInterval(() => {
+setInterval(async () => {
     blinki++
     let c = 0x000000
     if (state === "failure")
-        c = blinki % 2 === 0 ? 0xff0000 : 0x000000 // blink fast red
+        c = blinki % 2 === 0 ? 0x100000 : 0x000000 // blink fast red
     else if (state === "pending")
-        c = (blinki >> 1) % 2 === 0 ? 0xffa500 : 0x000000 // blink slow orange
-    else if (state === "success") c = 0x00ff00 // solid green
+        c = (blinki >> 1) % 2 === 0 ? 0x100500 : 0x000000 // blink slow orange
+    else if (state === "success") c = 0x000a00 // solid green
     else c = 0x000000 // dark if any error
+    await setStatusLight(c)
 }, 500)
 
 // query github every minute
