8592277d8e583c19a9d3d4d570dd49c680aea5fc
Michal Moskal <michal@moskal.me>
2023-06-08 15:58:30 -0700

    bump bytecode to v2.11.0

--- compiler/src/bytecode.ts
+++ compiler/src/bytecode.ts
@@ -105,9 +105,9 @@ export const OP_TYPES =
 
 export enum BinFmt {
     IMG_VERSION_MAJOR = 2,
-    IMG_VERSION_MINOR = 10,
-    IMG_VERSION_PATCH = 10,
-    IMG_VERSION = 0x20a000a,
+    IMG_VERSION_MINOR = 11,
+    IMG_VERSION_PATCH = 0,
+    IMG_VERSION = 0x20b0000,
     MAGIC0 = 0x53766544, // "DevS"
     MAGIC1 = 0xf1296e0a,
     NUM_IMG_SECTIONS = 10,
