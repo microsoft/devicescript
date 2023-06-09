775a046cd2d775b57995ae27b6225d46109477a6
pelikhan <jhalleux@microsoft.com>
2023-06-09 16:18:09 -0700

    character screen service working

--- packages/drivers/src/characterscreen.ts
+++ packages/drivers/src/characterscreen.ts
@@ -1,8 +1,10 @@
+import * as ds from "@devicescript/core"
 import { Server, ServerOptions, startServer } from "@devicescript/server"
 import {
     AsyncVoid,
     CharacterScreen,
     CharacterScreenTextDirection,
+    assert,
     isSimulator,
 } from "@devicescript/core"
 import { SSD1306Driver, SSD1306Options } from "./ssd1306"
@@ -16,8 +18,20 @@ class CharacterScreenServer extends Server {
     private readonly _render: () => AsyncVoid
     private readonly _columns: number
     private readonly _rows: number
+    /**
+     * Background color (palete index)
+     */
     background = 0
+    /**
+     * Foreground color (palete index)
+     */
     color = 1
+    // letter spacing
+    letterSpacing = 1
+    // line spacing
+    lineSpacing = 1
+    // outer margin
+    margin = 2
 
     constructor(
         options: {
@@ -27,13 +41,31 @@ class CharacterScreenServer extends Server {
         } & CharacterScreenOptions &
             ServerOptions
     ) {
-        super(CharacterScreen.spec, options)
+        super(ds.CharacterScreen.spec, options)
         this._image = options.image
         this._font = options.font
         this._render = options.render
         this._columns = options.columns
         this._rows = options.rows
+
+        if (this._columns === undefined)
+            this._columns = Math.floor(
+                (this._image.width - 2 * this.margin) /
+                    (this._font.charWidth + this.letterSpacing)
+            )
+        if (this._rows === undefined)
+            this._rows = Math.floor(
+                (this._image.height - 2 * this.margin) /
+                    (this._font.charHeight + this.lineSpacing)
+            )
+
+        if (this._rows === undefined || this._columns === undefined)
+            throw new Error("rows or columns is missing")
+
         this._message = ""
+
+        // clear screen
+        if (this._image) this._image.fill(this.background)
     }
 
     message() {
@@ -41,7 +73,9 @@ class CharacterScreenServer extends Server {
     }
 
     set_message(value: string) {
+        const old = this._message
         this._message = value || ""
+        if (old !== this._message) this.refresh()
     }
 
     columns() {
@@ -52,7 +86,13 @@ class CharacterScreenServer extends Server {
         return this._rows
     }
 
+    private refresh() {
+        if (this._render) this.render.start()
+    }
+
     private async render() {
+        assert(!!this._render)
+
         // paint image
         const img = this._image
         const columns = this._columns
@@ -62,13 +102,13 @@ class CharacterScreenServer extends Server {
         const c = this.color
         const f = this._font
 
+        const cw = this._font.charWidth
+        const ch = this._font.charHeight
+        const letterSpacing = this.letterSpacing
+        const lineSpacing = this.lineSpacing
+        const margin = this.margin
+
         img.fill(b)
-
-        const cw = 8
-        const ch = 10
-        const m = 1
-        const mo = 2
-
         let row = 0
         let column = 0
         for (const char of message) {
@@ -76,27 +116,28 @@ class CharacterScreenServer extends Server {
                 row += 1
                 column = 0
             } else {
-                if (column < columns)
+                if (column < columns) {
                     img.print(
                         char,
-                        column * (cw + m) + mo,
-                        row * (ch + m) + mo,
+                        column * (cw + letterSpacing) + margin,
+                        row * (ch + lineSpacing) + margin,
                         c,
                         f
                     )
+                }
                 column += 1
             }
             if (row >= rows) break
         }
 
         // flush buffer
-        this._render()
+        await this._render()
     }
 }
 
 export interface CharacterScreenOptions {
-    columns: number
-    rows: number
+    columns?: number
+    rows?: number
 }
 
 /**
@@ -106,16 +147,21 @@ export interface CharacterScreenOptions {
 export async function startSsd1306CharacterScreen(
     options: CharacterScreenOptions & SSD1306Options & ServerOptions
 ) {
-    if (isSimulator()) return new CharacterScreen()
-
     const font = fontForText("")
-    const ssd = new SSD1306Driver(options)
-    await ssd.init()
-
+    let image: Image
+    let render: () => AsyncVoid
+    if (isSimulator()) {
+        image = Image.alloc(options.width, options.height, 1)
+    } else {
+        const ssd = new SSD1306Driver(options)
+        await ssd.init()
+        image = ssd.image
+        render = async () => await ssd.show()
+    }
     const server = new CharacterScreenServer({
-        image: ssd.image,
+        image,
         font,
-        render: async () => await ssd.show(),
+        render,
         ...options,
     })
 
