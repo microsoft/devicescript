dfafbb8e8c338871aaa181ebe57a41e64c7a82bf
pelikhan <jhalleux@microsoft.com>
2023-06-10 19:41:10 -0700

    more canvas

--- packages/graphics/src/canvas.ts
+++ packages/graphics/src/canvas.ts
@@ -97,28 +97,65 @@ export class ImageRenderingContext {
         this.transformY += y
     }
 
-    // extras
+    /**
+     * Clears the canvas
+     */
     clear() {
         this.image.fill(0)
     }
+
+    /**
+     * Fills the canvas with the current fill color
+     */
     fill() {
         this.image.fill(this.fillColor)
     }
+
+    /**
+     * Fill a circle
+     * @param cx center x coordinate
+     * @param cy center y coordinate
+     * @param r radius
+     */
     fillCircle(cx: number, cy: number, r: number) {
         const x = this.transformX + cx
         const y = this.transformY + cy
         this.image.drawCircle(x, y, r, this.fillColor)
     }
+
+    /**
+     * Renders a circle outline
+     * @param cx x coordinate of the center
+     * @param cy y coordinate of the center
+     * @param r radius
+     */
     strokeCircle(cx: number, cy: number, r: number) {
         const x = this.transformX + cx
         const y = this.transformY + cy
         this.image.drawCircle(x, y, r, this.strokeColor)
     }
+
+    /**
+     * Renders a line from (x1, y1) to (x2, y2)
+     * @param x1 coordinate of the first point
+     * @param y1 coordinate of the first point
+     * @param x2 coordinate of the second point
+     * @param y2 coordinate of the second point
+     */
     strokeLine(x1: number, y1: number, x2: number, y2: number) {
         const tx1 = this.transformX + x1
         const ty1 = this.transformY + y1
         const tx2 = this.transformX + x2
         const ty2 = this.transformY + y2
         this.image.drawLine(tx1, ty1, tx2, ty2, this.strokeColor)
+    }
+
+    /**
+     * Resets the state of the canvas context
+     */
+    reset() {
+        this.resetTransform()
+        this.fillColor = 1
+        this.strokeColor = 1
     }
 }
