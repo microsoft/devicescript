45d99524426354c15291fb28f19a3327d70ff1b3
pelikhan <jhalleux@microsoft.com>
2023-06-26 12:25:55 -0700

    make palette optional

--- packages/graphics/src/display.ts
+++ packages/graphics/src/display.ts
@@ -2,7 +2,10 @@ import { Image } from "./image"
 import { Palette } from "./palette"
 
 export interface Display {
-    palette: Palette
+    /**
+     * Palette for the display. For monochrome displays, this is a 2-color palette.
+     */
+    palette?: Palette
     image: Image
     show(): Promise<void>
     init(): Promise<void>
