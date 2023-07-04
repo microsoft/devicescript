7e3ed032cee166ca3c53824065e12186feefab7b
pelikhan <jhalleux@microsoft.com>
2023-07-03 19:30:36 -0500

    add comments to display

--- packages/graphics/src/display.ts
+++ packages/graphics/src/display.ts
@@ -1,9 +1,24 @@
 import { Image } from "./image"
 import { Palette } from "./palette"
 
+/**
+ * A display driver
+ */
 export interface Display {
+    /**
+     * Color palette supported by the display
+     */
     palette: Palette
+    /**
+     * Buffered image rendered when show is called
+     */
     image: Image
+    /**
+     * Renders the image to hardware
+     */
     show(): Promise<void>
+    /**
+     * Initializes the display. Should be callable multiple timews.
+     */
     init(): Promise<void>
 }
