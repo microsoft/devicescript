dfda979da3082319260cdaf9f7c27ba5b5c073e8
Michal Moskal <michal@moskal.me>
2023-07-07 10:27:23 -0700

    type-safe props

--- packages/ui/src/ui.tsx
+++ packages/ui/src/ui.tsx
@@ -1,14 +1,18 @@
 import * as ds from "@devicescript/core"
 import { Image, ImageContext } from "@devicescript/graphics"
 
-export abstract class BaseElement<T> implements JSX.Element {
-    constructor(props: T) {
-        Object.assign(this, props)
-    }
+/**
+ * Type-safe wrapper for Object.assign()
+ */
+function setup<T, S extends T>(self: S, props: T) {
+    Object.assign(self, props)
+}
+
+export abstract class BaseElement implements JSX.Element {
     abstract render(ctx: ImageContext): void
 }
 
-export abstract class ParentElement<T> extends BaseElement<T> {
+export abstract class ParentElement extends BaseElement {
     children?: JSX.Element | JSX.Element[]
 
     renderChildren(ctx: ImageContext) {
@@ -21,63 +25,83 @@ export abstract class ParentElement<T> extends BaseElement<T> {
     }
 }
 
-export class Translate extends ParentElement<{ x: number, y: number } & JSX.BaseProps> {
+export class Translate extends ParentElement {
     x: number
     y: number
 
+    constructor(props: { x?: number, y?: number } & JSX.BaseProps) {
+        super()
+        setup(this, props)
+    }
+
     render(ctx: ImageContext): void {
         ctx.save()
-        ctx.translate(this.x, this.y)
+        ctx.translate(this.x ?? 0, this.y ?? 0)
         this.renderChildren(ctx)
         ctx.restore()
     }
 }
 
-export class Text extends BaseElement<{ children: string }> {
+export class Text extends BaseElement {
     children: string
+
+    constructor(props: { children: string }) {
+        super()
+        setup(this, props)
+    }
 
     render(ctx: ImageContext): void {
         ctx.fillText(this.children, 0, 0)
     }
 }
 
-export class Rect extends BaseElement<{ w: number, h: number }> {
-    w: number
-    h: number
+export class Rect extends BaseElement {
+    width: number
+    height: number
+
+    constructor(props: { width: number, height: number }) {
+        super()
+        setup(this, props)
+    }
 
     render(ctx: ImageContext): void {
-        ctx.strokeRect(0, 0, this.w, this.h)
+        ctx.strokeRect(0, 0, this.width, this.height)
     }
 }
 
 export class FillRect extends Rect {
     override render(ctx: ImageContext): void {
-        ctx.fillRect(0, 0, this.w, this.h)
+        ctx.fillRect(0, 0, this.width, this.height)
     }
 }
 
-export class Fragment extends ParentElement<JSX.BaseProps> {
+export class Fragment extends ParentElement {
+    constructor(props: JSX.BaseProps) {
+        super()
+        setup(this, props)
+    }
+
     render(ctx: ImageContext): void {
         this.renderChildren(ctx)
     }
 }
 
 
-export function HorizontalGauge(props: { w: number, h: number, used: number }): JSX.Element {
-    const { w, h, used } = props
+export function HorizontalGauge(props: { width: number, height: number, used: number }): JSX.Element {
+    const { width, height, used } = props
     return <>
-        <Rect w={w} h={h}></Rect>
+        <Rect width={width} height={height}></Rect>
         <Translate x={2} y={2}>
-            <FillRect w={Math.round((w - 4) * used)} h={h - 4}></FillRect>
+            <FillRect width={Math.round((width - 4) * used)} height={height - 4}></FillRect>
         </Translate>
     </>
 }
 
-export function PercHorizontalGauge(props: { w: number, h: number, used: number }): JSX.Element {
-    const { w, h, used } = props
+export function PercHorizontalGauge(props: { width: number, height: number, used: number }): JSX.Element {
+    const { width, used } = props
     return <>
         <HorizontalGauge {...props} />
-        <Translate x={w + 2} y={0}>
+        <Translate x={width + 2} y={0}>
             <Text>{Math.round(used * 100) + "%"}</Text>
         </Translate>
     </>
