d5ec16ceff96d97d6caec0244eafb2bf98a82320
pelikhan <jhalleux@microsoft.com>
2023-07-07 09:06:05 +0200

    fix rendering

--- packages/runtime/src/valuedashboard.ts
+++ packages/runtime/src/valuedashboard.ts
@@ -1,19 +1,57 @@
 import { CharacterScreen } from "@devicescript/core"
 
+function roundWithPrecision(x: number, digits: number): number {
+    const EPSILON = 2.220446049250313e-16
+    digits = digits | 0
+    if (digits <= 0) return Math.round(x)
+    if (x === 0) return 0
+    let r = 0
+    while (r === 0 && digits < 21) {
+        const d = Math.pow(10, digits++)
+        r = Math.round(x * d + EPSILON) / d
+    }
+    return r
+}
+
+export interface ValueDomain {
+    unit?: string
+    digits?: number
+}
+
 /**
  * Renders a set of name, values on a character display
  */
-export class ValueDashbaord {
+export class ValueDashboard<T extends Record<string, ValueDomain>> {
     /**
      * Map of name, values
      */
-    readonly values: Record<string, string | number | boolean> = {}
+    values: Record<string, string | number | boolean> = {}
+
+    /**
+     * Map of name to number domains
+     */
+    domains: Record<string, ValueDomain> = {}
+
     /**
      * Line offset value
      */
     public offset = 0
 
-    constructor(readonly screen: CharacterScreen) {}
+    constructor(
+        readonly screen: CharacterScreen,
+        domains: Record<string, ValueDomain>
+    ) {
+        this.domains = domains || {}
+    }
+
+    private renderNumber(name: string, value: number) {
+        const domain = this.domains[name] || {}
+        if (domain.digits !== undefined)
+            value = roundWithPrecision(value, domain.digits)
+        let r = value + ""
+        if (domain.unit) r += domain.unit
+        return r
+    }
 
     /**
      * Renders the current values to the character display
@@ -34,13 +72,18 @@ export class ValueDashbaord {
             const sv =
                 typeof value === "string"
                     ? value
+                    : typeof value === "number"
+                    ? this.renderNumber(name, value)
                     : typeof value === "boolean"
-                    ? "v"
+                    ? value
+                        ? "v"
+                        : "x"
                     : value + ""
             const msv = sv.slice(0, mvl)
             let line = name
-            while (line.length - sv.length > 1) line += " "
+            while (line.length + msv.length + 1 <= columns) line += " "
             line += msv
+            lines.push(line)
         }
         const msg = lines.join("\n")
         await this.screen.message.write(msg)
--- packages/sampleprj/src/mainvaluedashboard.ts
+++ packages/sampleprj/src/mainvaluedashboard.ts
@@ -1,12 +1,15 @@
 import { CharacterScreen, Humidity, Temperature } from "@devicescript/core"
-import { ValueDashbaord } from "@devicescript/runtime"
+import { ValueDashboard } from "@devicescript/runtime"
 
 const temperature = new Temperature()
 const humidity = new Humidity()
 const screen = new CharacterScreen()
-const dashboard = new ValueDashbaord(screen)
+const dashboard = new ValueDashboard(screen, {
+    temp: { digits: 1, unit: "C" },
+    humi: { digits: 1, unit: "%" },
+})
 setInterval(async () => {
-    dashboard.values["temperature"] = await temperature.reading.read()
-    dashboard.values["humidity"] = await humidity.reading.read()
+    dashboard.values["temp"] = await temperature.reading.read()
+    dashboard.values["humi"] = await humidity.reading.read()
     await dashboard.show()
 }, 1000)
