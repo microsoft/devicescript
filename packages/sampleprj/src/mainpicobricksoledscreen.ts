1878ed2675fd962366c9e2832b689742144e5bcf
pelikhan <jhalleux@microsoft.com>
2023-07-21 12:11:23 +0200

    updated sample

--- packages/sampleprj/src/mainpicobricksoledscreen.ts
+++ packages/sampleprj/src/mainpicobricksoledscreen.ts
@@ -2,11 +2,12 @@ import { pins, board } from "@dsboard/pico_bricks"
 import { SSD1306Driver, startIndexedScreen } from "@devicescript/drivers"
 
 const screen = await startIndexedScreen(
-    new SSD1306Driver({ width: 64, height: 48 })
+    new SSD1306Driver({ width: 128, height: 64 })
 )
 let i = 0
 setInterval(async () => {
     console.log(i)
+    screen.image.fill(0)
     screen.image.print(`Hello world! ${i++}`, 3, 10)
     await screen.show()
 }, 1000)
