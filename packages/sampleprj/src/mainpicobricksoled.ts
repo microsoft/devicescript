8368737ff2a70f7cfdf602fdb1ae21672ca43c19
pelikhan <jhalleux@microsoft.com>
2023-07-21 12:39:18 +0200

    updated pico_w samples

--- packages/sampleprj/src/mainpicobricksoled.ts
+++ packages/sampleprj/src/mainpicobricksoled.ts
@@ -1,7 +1,17 @@
-import { pins, board } from "@dsboard/pico_bricks"
+import { pins, board } from "@dsboard/pico_w"
 import { SSD1306Driver } from "@devicescript/drivers"
+import { configureHardware } from "@devicescript/servers"
 
-const ssd = new SSD1306Driver({ width: 64, height: 48 })
+configureHardware({
+    scanI2C: false,
+    i2c: {
+        pinSCL: pins.GP5,
+        pinSDA: pins.GP4,
+        kHz: 200,
+    },
+})
+
+const ssd = new SSD1306Driver({ width: 128, height: 64, devAddr: 0x3c })
 await ssd.init()
 ssd.image.print("Hello world!", 3, 10)
 await ssd.show()
--- packages/sampleprj/src/mainpicobricksoledscreen.ts
+++ packages/sampleprj/src/mainpicobricksoledscreen.ts
@@ -1,9 +1,19 @@
-import { pins, board } from "@dsboard/pico_bricks"
+import { pins, board } from "@dsboard/pico_w"
 import { SSD1306Driver, startIndexedScreen } from "@devicescript/drivers"
 import { delay } from "@devicescript/core"
+import { configureHardware } from "@devicescript/servers"
+
+configureHardware({
+    scanI2C: false,
+    i2c: {
+        pinSCL: pins.GP5,
+        pinSDA: pins.GP4,
+        kHz: 200,
+    },
+})
 
 const screen = await startIndexedScreen(
-    new SSD1306Driver({ width: 128, height: 64 })
+    new SSD1306Driver({ width: 128, height: 64, devAddr: 0x3c })
 )
 const image = screen.image
 image.fill(0)
@@ -12,6 +22,6 @@ for (let x = 0; x < image.width; x += 2) {
     for (let y = 0; y < image.height; y += 2) {
         image.set(x, y, 1)
         await screen.show()
-        await delay(10)
+        await delay(100)
     }
 }
