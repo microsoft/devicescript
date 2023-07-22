49369ee534552c21fe07b4dafec2187a652f1d45
pelikhan <jhalleux@microsoft.com>
2023-07-22 09:55:47 +0200

    add large number of LEDs example

--- packages/sampleprj/src/mainled.ts
+++ packages/sampleprj/src/mainled.ts
@@ -4,17 +4,30 @@ import { startLed } from "@devicescript/drivers"
 
 const led = await startLed({
     length: 12,
-    variant: LedVariant.Strip
+    variant: LedVariant.Strip,
 })
 const pixels = await led.buffer()
+
+const led2 = await startLed({
+    length: 256,
+    variant: LedVariant.Strip,
+})
+const pixels2 = await led.buffer()
 
 setInterval(async () => {
     pixels.setAll(rgb(255, 0, 0))
     await led.show()
+
+    pixels2.setAll(rgb(0, 0, 255))
+    await led2.show()
+
     await delay(1000)
 
     pixels.setAll(0)
     await led.show()
-    await delay(500)
 
+    pixels2.setAll(0)
+    await led2.show()
+
+    await delay(500)
 }, 500)
