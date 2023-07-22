621c7792e384ae6934002db93f6db890f4c6e5e8
pelikhan <jhalleux@microsoft.com>
2023-07-22 06:16:02 +0200

    fix gpiorelay sample

--- packages/sampleprj/src/mainblinky.ts
+++ packages/sampleprj/src/mainblinky.ts
@@ -1,11 +1,21 @@
+/**
+ * @file Blinks the status light using setInterval
+ *
+ * @remarks
+ *
+ * `setStatusLight` is a generic API to control the on-board status LED of a device.
+ * It can be used to provide visual feedback about the state of a program.
+ * Just remember that DeviceScript may also use the status light to inform about states
+ * of the device.
+ */
 import { delay } from "@devicescript/core"
 import { setStatusLight } from "@devicescript/runtime"
 
 setInterval(async () => {
-    console.log('off')
+    // turn off
     await setStatusLight(0)
     await delay(1000)
-    console.log('on')
+    // turn on
     await setStatusLight(0x0f0f0f)
     await delay(1000)
 }, 10)
