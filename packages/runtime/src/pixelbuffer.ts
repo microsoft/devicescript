0607bddff198675cb1ba136b241f45e3078c64ed
pelikhan <jhalleux@microsoft.com>
2023-07-31 20:57:15 +0200

    support palette interpolation in bargraph

--- packages/runtime/src/pixelbuffer.ts
+++ packages/runtime/src/pixelbuffer.ts
@@ -188,7 +188,11 @@ export function fillBarGraph(
         /**
          * Color used when the value is 0
          */
-        zeroColor?: number
+        zeroColor?: number,
+        /**
+         * Palette to interpolate color from
+         */
+        palette?: Palette
     }
 ) {
     if (high <= 0) {
@@ -201,6 +205,7 @@ export function fillBarGraph(
     value = Math.abs(value)
     const n = pixels.length
     const n1 = n - 1
+    const palette = options?.palette
     let v = Math.idiv(value * n, high)
     if (v === 0) {
         const zeroColor = options?.zeroColor
@@ -209,8 +214,14 @@ export function fillBarGraph(
     } else {
         for (let i = 0; i < n; ++i) {
             if (i <= v) {
-                const b = Math.idiv(i * 255, n1)
-                pixels.setRgbAt(i, b, 0, 255 - b)
+                if (palette) {
+                    const c = palette.interpolate(i / (n - 1))
+                    pixels.setAt(i, c)
+                } else {
+                    const b = Math.idiv(i * 255, n1)
+                    pixels.setRgbAt(i, b, 0, 255 - b)
+
+                }
             } else pixels.setRgbAt(i, 0, 0, 0)
         }
     }
