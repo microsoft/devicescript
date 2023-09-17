13360861c835ede1c172093aabc91bcdafc78d0a
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2023-09-17 21:48:49 +0530

    Optimizing Performance and Resource Utilization Through Concurrent Async Execution ✈ (#623)

--- scripts/processvideos.mjs
+++ scripts/processvideos.mjs
@@ -4,7 +4,7 @@ import "zx/globals"
 
 await cd("website/static/videos")
 const files = fs.readdirSync(".").filter(f => f.endsWith(".mp4"))
-for (const file of files) {
+await Promise.all(files.map((file) => {
     const jpg = file.substring(0, file.length - 3) + "jpg"
-    if (!fs.existsSync(jpg)) await $`ffmpeg -y -i ${file} -frames:v 1 ${jpg}`
-}
+    if (!fs.existsSync(jpg)) return $`ffmpeg -y -i ${file} -frames:v 1 ${jpg}`
+}))
