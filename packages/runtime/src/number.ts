1b83ef92be1cf8fa1a399878e9862c1af80a92d2
Piotr Kasperski <piotr.kasperski94@gmail.com>
2023-12-03 23:16:13 +0100

    Add Number.isSafeInteger

--- packages/runtime/src/number.ts
+++ packages/runtime/src/number.ts
@@ -20,6 +20,10 @@ export class Number {
     static isNaN(number: unknown): boolean {
         return isNaN(number as number)
     }
+    /**
+     * The boolean value true if the given value is a number that is a safe integer. Otherwise false.
+     * @param The value to be tested for being a safe integer.
+     */
     static isSafeInteger(number: unknown): boolean {
         return (
             number >= -(2 ** 53 - 1) &&
