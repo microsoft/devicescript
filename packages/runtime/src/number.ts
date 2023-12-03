dcebc45cdd673a18b5639f2c01b4a95b7e924595
Piotr Kasperski <piotr.kasperski94@gmail.com>
2023-12-03 22:34:12 +0100

    Number.isFinite

--- packages/runtime/src/number.ts
+++ packages/runtime/src/number.ts
@@ -20,4 +20,16 @@ export class Number {
     static isNaN(number: unknown): boolean {
         return isNaN(number as number)
     }
+    /**
+     * Returns tru if the passed value is finite number, false otherwise.
+     * @param number A numeric value
+     */
+    static isFinite(number: unknown): boolean {
+        return (
+            number !== Infinity &&
+            number !== -Infinity &&
+            typeof number === "number" &&
+            !isNaN(number)
+        )
+    }
 }
