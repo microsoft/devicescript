3d160729e33dc7dc68321ad12aae467f73ad367c
Piotr Kasperski <piotr.kasperski94@gmail.com>
2023-12-03 23:12:29 +0100

    Add Number.isSafeInteger

--- packages/runtime/src/number.ts
+++ packages/runtime/src/number.ts
@@ -20,4 +20,12 @@ export class Number {
     static isNaN(number: unknown): boolean {
         return isNaN(number as number)
     }
+    static isSafeInteger(number: unknown): boolean {
+        return (
+            number >= -(2 ** 53 - 1) &&
+            number <= 2 ** 53 - 1 &&
+            typeof number === "number" &&
+            number === Math.round(number)
+        )
+    }
 }
