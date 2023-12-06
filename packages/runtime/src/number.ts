63a0ac93b84f1a8a2f1a1e622bb4a6a3995b8e2b
Piotr Kasperski <piotr.kasperski94@gmail.com>
2023-12-06 21:29:48 +0100

    work for base digits

--- packages/runtime/src/number.ts
+++ packages/runtime/src/number.ts
@@ -21,19 +21,22 @@ export class Number {
         return isNaN(number as number)
     }
     static parseInt(string: unknown, radix?: unknown): number {
-        if (!radix) radix = 10
+        if (!radix || radix === 10) return parseInt(string as string)
         if (radix < 2 || radix < 2) return NaN
-        const validChars: string = (
-            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" as string
-        ).slice(radix as number)
-
+        const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const validChars = chars.slice(0, radix as number)
         const inputUpperCase: string = (string as string).toUpperCase()
         let result: number = 0
-        for (const char of inputUpperCase) {
-            const charValue = validChars.indexOf(char)
+        for (let index = 0; index < inputUpperCase.length; index++) {
+            const charValue = validChars.indexOf(inputUpperCase[index])
             if (charValue === -1) return NaN
             result = result * (radix as number) + charValue
         }
+        //for (const char of inputUpperCase) {
+        //    const charValue = validChars.indexOf(char)
+        //    if (charValue === -1) return NaN
+        //    result = result * (radix as number) + charValue
+        //}
         return result
     }
 }
